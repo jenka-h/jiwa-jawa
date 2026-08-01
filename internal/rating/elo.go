8a322cf718f9c5ee19e5506a6834944bc3d90669
Real <jenniferkhang07@gmail.com>
2026-08-02 02:32:30 +0700

    feat: elo

--- internal/rating/elo.go
+++ internal/rating/elo.go
@@ -1,6 +1,6 @@
 package rating
 
-import err "jiwa-jawa/internal/error"
+import "math"
 
 // Elo implements a basic Elo-style rating system.
 type Elo struct {
@@ -9,15 +9,39 @@ type Elo struct {
 
 // NewElo creates an Elo rating system.
 func NewElo(kFactor float64) *Elo {
-	return nil
+	return &Elo{KFactor: kFactor}
 }
 
 // ExpectedScore returns player a's expected score against player b.
 func (e *Elo) ExpectedScore(a PlayerRating, b PlayerRating) float64 {
-	return 0
+	return 1 / (1 + math.Pow(10, (b.Rating-a.Rating)/400))
 }
 
 // Update updates ratings after one game.
 func (e *Elo) Update(a PlayerRating, b PlayerRating, result Result) (PlayerRating, PlayerRating, error) {
-	return PlayerRating{}, PlayerRating{}, err.ErrNotImplemented
+	expectedA := e.ExpectedScore(a, b)
+	expectedB := e.ExpectedScore(b, a)
+
+	scoreA := float64(result)
+	scoreB := 1 - scoreA
+
+	a.Rating = a.Rating + e.KFactor*(scoreA-expectedA)
+	b.Rating = b.Rating + e.KFactor*(scoreB-expectedB)
+
+	a.Games++
+	b.Games++
+
+	switch result {
+	case Win:
+		a.Wins++
+		b.Losses++
+	case Draw:
+		a.Draws++
+		b.Draws++
+	case Loss:
+		a.Losses++
+		b.Wins++
+	}
+
+	return a, b, nil
 }
